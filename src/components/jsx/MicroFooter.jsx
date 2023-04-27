16fd7f32a707e4a07908eb900c3f0bd1febc3b53
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2023-04-27 07:57:22 +0000

    Update to work on iOS

--- src/components/jsx/MicroFooter.jsx
+++ src/components/jsx/MicroFooter.jsx
@@ -6,17 +6,23 @@ export default function MicroFooter() {
 
   return (
     <div className="flex flex-col sm:flex-row justify-between items-center absolute bottom-2 left-2 w-[calc(100%-1rem)] z-30">
-      <div className="text-center mb-4 sm:mb-0 xs:text-left mr-auto">© 2023 Entropic Software. All rights reserved.</div>
+      <div
+        className={`text-center ${
+          router.asPath !== "/" && "mb-2"
+        } sm:mb-0 xs:text-left sm:mr-auto`}
+      >
+        © 2023 Entropic Software. All rights reserved.
+      </div>
       <Link href={"/"} className={router.asPath === "/" && "hidden"}>
         <div className="relative top-0.5">
-          <span 
-            className="text-lg bioweapon-font relative mb-8 xl:mb-0 mr-4"
+          <span
+            className={`text-lg bioweapon-font relative xl:mb-0 mr-4`}
             style={{ fontWeight: "900", lineHeight: 1 }}
           >
             ENTROPIC
             <span className="font-mirror-layer" />
           </span>
-          <span 
+          <span
             className="text-lg bioweapon-font relative"
             style={{ fontWeight: "900", lineHeight: 1 }}
           >
@@ -26,5 +32,5 @@ export default function MicroFooter() {
         </div>
       </Link>
     </div>
-  )
+  );
 }
